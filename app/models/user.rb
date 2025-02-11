eb82b255e3b8fcdb0c3bb68a937eecc7cb5efe16
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-11 19:49:09 +0200

    Add User.personality enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
-  has_many :created_activities, class_name: 'Activity', foreign_key: 'user_id'
+  has_many :created_activities, class_name: "Activity", foreign_key: "user_id"
   has_many :participants
   has_many :joined_activities, through: :participants, source: :activity
+
+  enum :personality, {
+    very_introverted: 1,
+    introverted: 2,
+    slightly_introverted: 3,
+    ambiverted: 4,
+    slightly_extraverted: 5,
+    extraverted: 6,
+    very_extraverted: 7
+  }
 end
