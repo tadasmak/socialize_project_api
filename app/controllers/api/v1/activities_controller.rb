a4560934da1df00df6ddbd045312091821695fdb
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-12 17:32:45 +0200

    Create create activity POST /activities endpoint

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -3,4 +3,27 @@ class Api::V1::ActivitiesController < ApplicationController
     activities = Activity.all
     render json: activities
   end
+
+  def create
+    @activity = Activity.new(activity_params)
+
+    if @activity.save
+      render status: :created, json: @activity
+    else
+      render status: :bad_request, json: { errors: @activity.errors }
+    end
+  end
+
+  private
+
+  def activity_params
+    params.permit(
+      :title,
+      :description,
+      :location,
+      :start_time,
+      :max_participants,
+      :user_id
+    )
+  end
 end
