56604572223d715853ce117c54c02746d8465859
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-12 21:00:48 +0200

    Separate permitted params for create and update activity endpoints

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::ActivitiesController < ApplicationController
   end
 
   def create
-    activity = Activity.new(activity_params)
+    activity = Activity.new(activity_create_params)
 
     if activity.save
       render status: :created, json: activity
@@ -21,22 +21,22 @@ class Api::V1::ActivitiesController < ApplicationController
 
   def update
     activity = Activity.find(params[:id])
-    activity.update(activity_params)
+    activity.update(activity_update_params)
 
     render status: :ok, json: activity
   end
 
   private
 
-  def activity_params
-    params.permit(
-      :id,
-      :title,
-      :description,
-      :location,
-      :start_time,
-      :max_participants,
-      :user_id
-    )
+  def activity_create_params
+    params.require(:activity).permit(permitted_activity_attributes + [ :user_id ])
+  end
+
+  def activity_update_params
+    params.require(:activity).permit(permitted_activity_attributes)
+  end
+
+  def permitted_activity_attributes
+    [ :title, :description, :location, :start_time, :max_participants ]
   end
 end
