da791e142d49fa3ae2eb5aaeff5ab5a66217b402
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-17 18:50:16 +0200

    Generate API Swagger documentation files

---
+++ spec/api_docs/api/v1/activities_spec.rb
@@ -0,0 +1,74 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/activities', type: :request do
+
+  path '/api/v1/activities' do
+
+    get('List activities') do
+      tags 'Activities'
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('Create activity') do
+      tags 'Activities'
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/activities/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('Show activity') do
+      tags 'Activities'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    put('Update activity') do
+      tags 'Activities'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
