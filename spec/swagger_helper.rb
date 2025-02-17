da2e880768751e4652e80a1d9a7c2d171b13e0e8
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-17 18:23:17 +0200

    Replace .yaml Swagger file with .json and specify documentation

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -15,7 +15,7 @@ RSpec.configure do |config|
   # document below. You can override this behavior by adding a openapi_spec tag to the
   # the root example_group in your specs, e.g. describe '...', openapi_spec: 'v2/swagger.json'
   config.openapi_specs = {
-    'v1/swagger.yaml' => {
+    'v1/swagger.json' => {
       openapi: '3.0.1',
       info: {
         title: 'API V1',
@@ -24,12 +24,8 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'https://{defaultHost}',
-          variables: {
-            defaultHost: {
-              default: 'www.example.com'
-            }
-          }
+          url: "#{ENV.fetch('SERVER_URL')}",
+          variables: {}
         }
       ]
     }
@@ -39,5 +35,5 @@ RSpec.configure do |config|
   # The openapi_specs configuration option has the filename including format in
   # the key, this may want to be changed to avoid putting yaml in json files.
   # Defaults to json. Accepts ':json' and ':yaml'.
-  config.openapi_format = :yaml
+  config.openapi_format = :json
 end
