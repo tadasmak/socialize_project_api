f7229622b24557a95cdacf2c7aad81ce8e375a52
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-19 16:36:23 +0200

    Define activity test data in FactoryBot

--- test/factories/activities.rb
+++ test/factories/activities.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :activity do
-    
+    title { Faker::Lorem.sentence }
+    description { Faker::Lorem.paragraph }
+    location { Faker::Address.city }
+    start_time { Faket::Time.forward(days: 5, period: :evening) }
+    max_participants { rand(2..8) }
+
+    association :user
   end
 end
