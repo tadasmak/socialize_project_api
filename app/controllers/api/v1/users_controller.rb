745378d556637c27aa98c898e6cb80e5e9546b0d
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-20 15:42:27 +0200

    Create POST /users endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::UsersController < ApplicationController
     user = User.find(params[:id])
     render status: :ok, json: user
   end
+
+  def create
+    user = User.create(user_create_params)
+
+    if user.save
+      render status: :created, json: user
+    else
+      render status: :bad_request, json: { errors: user.errors }
+    end
+  end
+
+  private
+
+  def user_create_params
+    params.permit([ :email, :username, :personality ])
+  end
 end
