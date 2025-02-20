d43a1c3e4393cc61bfb4298dc6faa3e449bd9cce
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-20 17:11:57 +0200

    Add Activity model constraints to title, description, location, start_time and max_participants fields

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,17 @@ class Activity < ApplicationRecord
   belongs_to :user
   has_many :participants
   has_many :users, through: :participants
+
+  validates :title, presence: true, format: { with: /\A[a-zA-Z0-9\s]+\z/, message: "only allows letters and numbers" },
+                                    length: { minimum: 8, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 40, maximum: 1000 }
+  validates :location, presence: true, length: { minimum: 4, maximum: 100 }
+  validate :start_time_constraint
+  validates :max_participants, presence: true, inclusion: { in: 2..8, message: "must be between 2 and 8" }
+
+  def start_time_constraint
+    errors.add(:start_time, "start_time does not exist") if start_time.blank?
+    errors.add(:start_time, "start time should be in the future") if start_time < Time.now
+    errors.add(:start_time, "must be no further that 1 month in the future") if start_time > 1.month.from_now
+  end
 end
