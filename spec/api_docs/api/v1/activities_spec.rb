fb206163e7498fd0f5c71a88d8682f7f478abf92
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-20 19:11:48 +0200

    Define parameters in activities API Swagger

--- spec/api_docs/api/v1/activities_spec.rb
+++ spec/api_docs/api/v1/activities_spec.rb
@@ -18,6 +18,13 @@ RSpec.describe 'Activities API V1', type: :request do
 
     post('Create activity') do
       tags 'Activities'
+      parameter name: 'title', in: :body, type: :string, required: true, description: "Consists of 8-100 symbols without any special characters"
+      parameter name: 'description', in: :body, type: :text, required: true, description: "Consists of 40-1000 symbols without any special characters"
+      parameter name: 'location', in: :body, type: :string, required: true, description: "Consists of 4-100 symbols without any special characters"
+      parameter name: 'start_time', in: :body, type: :string, required: true, description: "Must be in datetime format and no further than 1 month in the future"
+      parameter name: 'max_participants', in: :body, type: :integer, required: true, description: "Accepts from 2 to 8"
+      parameter name: 'user_id', in: :body, type: :integer, required: true, description: "Activity owner's user id"
+
       response(201, 'created') do
         after do |example|
           example.metadata[:response][:content] = {
@@ -69,6 +76,12 @@ RSpec.describe 'Activities API V1', type: :request do
 
     put('Update activity') do
       tags 'Activities'
+      parameter name: 'title', in: :body, type: :string, description: "Consists of 8-100 symbols without any special characters"
+      parameter name: 'description', in: :body, type: :text, description: "Consists of 40-1000 symbols without any special characters"
+      parameter name: 'location', in: :body, type: :string, description: "Consists of 4-100 symbols without any special characters"
+      parameter name: 'start_time', in: :body, type: :string, description: "Must be in datetime format and no further than 1 month in the future"
+      parameter name: 'max_participants', in: :body, type: :integer, description: "Accepts from 2 to 8"
+
       response(200, 'successful') do
         let(:id) { '123' }
 
