313d121318c426b0c31bf0b91e210c0601a99a64
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-24 17:04:59 +0200

    Generate RSpec request test file for Users API

---
+++ spec/api_docs/api/v1/users_spec.rb
@@ -0,0 +1,83 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/users', type: :request do
+  path '/api/v1/users' do
+    post('Create user') do
+      tags 'Users'
+      response(201, 'created') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/users/{id}' do
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('Show user') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    patch('Update user') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    put('Update user') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('Delete user') do
+      response(204, 'no content') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
