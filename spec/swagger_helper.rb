37db0eade412585cfe45140ff21ec8de02802107
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-24 22:47:33 +0200

    Define Activity, Participant and User components schemas in swagger_helper.rb

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -21,13 +21,51 @@ RSpec.configure do |config|
         title: 'API V1',
         version: 'v1'
       },
-      paths: {},
       servers: [
         {
-          url: "#{ENV.fetch('SERVER_URL')}",
+          url: "#{ENV.fetch('SERVER_URL', 'http://localhost:3000')}",
           variables: {}
         }
-      ]
+      ],
+      paths: {},
+      components: {
+        schemas: {
+          Activity: {
+            type: :object,
+            properties: {
+              id: { type: :integer, example: 1 },
+              title: { type: :string, example: 'Basketball Game' },
+              description: { type: :string, example: 'A 5v5 basketball game for beginners' },
+              location: { type: :string, example: 'Crypto.com arena, Los Angeles' },
+              start_time: { type: :string, format: 'date-time', example: '2028-03-24T14:15:22Z' },
+              max_participants: { type: :integer, example: 5 },
+              user: { '$ref' => '#/components/schemas/User' },
+              participants: {
+                type: :array,
+                items: { '$ref' => '#/components/schemas/User' }
+              }
+            }
+          },
+          Participant: {
+            type: :object,
+            properties: {
+              id: { type: :integer, example: 1 },
+              user_id: { type: :integer, example: 2 },
+              activity_id: { type: :integer, example: 15 },
+              created_at: { type: :string, format: 'date-time', example: '2024-02-24T14:15:22Z' }
+            }
+          },
+          User: {
+            type: :object,
+            properties: {
+              id: { type: :integer, example: 14 },
+              email: { type: :string, example: 'user@example.com' },
+              username: { type: :string, example: 'john_doe' },
+              personality: { type: :integer, example: 5 }
+            }
+          }
+        }
+      }
     }
   }
 
