48f3a496b94a35d079cc79ea5dc6304bbba159f5
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-25 16:45:40 +0200

    Refactor participants API doc spec file

--- spec/api_docs/api/v1/participants_spec.rb
+++ spec/api_docs/api/v1/participants_spec.rb
@@ -7,27 +7,11 @@ RSpec.describe 'api/v1/participants', type: :request do
       consumes 'application/json'
       produces 'application/json'
 
-      request_body do
-        content 'application/json' => {
-          schema: {
-            type: :object,
-            properties: {
-              user_id: { type: :integer, example: 5 },
-              activity_id: { type: :integer, example: 1 }
-            },
-            required: %w[user_id activity_id]
-          }
-        }
-      end
+      parameter name: :user_id, in: :body, required: true, schema: { type: :integer, example: 5 }
+      parameter name: :activity_id, in: :body, required: true, schema: { type: :integer, example: 1 }
 
       response(201, 'created') do
-        schema type: :object,
-               properties: {
-                  id: { type: :integer, example: 10 },
-                  user_id: { type: :integer, example: 5 },
-                  activity_id: { type: :integer, example: 1 },
-                  created_at: { type: :string, format: 'date-time', example: '2028-03-24T14:15:22Z' }
-               }
+        schema '$ref' => '#/components/schemas/Participant'
 
         let(:valid_params) { { user_id: 5, activity_id: 1 } }
         run_test!
