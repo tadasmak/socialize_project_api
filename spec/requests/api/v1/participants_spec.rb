32a73532cd7e05cea7b9ae02026c8fb3164cdb8e
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-26 20:24:29 +0200

    Improve Participant API RSpec test file

--- spec/requests/api/v1/participants_spec.rb
+++ spec/requests/api/v1/participants_spec.rb
@@ -6,10 +6,13 @@ RSpec.describe "Participants API V1", type: :request do
 
   describe "POST /api/v1/participants" do
     it "joins an activity" do
-      post api_v1_participants_path,
-      params: { user_id: user.id, activity_id: activity.id }
+      expect {
+        post api_v1_participants_path,
+        params: { user_id: user.id, activity_id: activity.id },
+        as: :json
+      }.to change(Participant, :count).by(1)
+
       expect(response).to have_http_status(:success)
-      expect(Participant.count).to eq(1)
     end
   end
 end
