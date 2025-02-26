77a19b489253cff16ebf3a40258bcfbe2ed0538b
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-26 20:15:09 +0200

    Fix description and location validation in Activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -7,10 +7,10 @@ class Activity < ApplicationRecord
                     format: { without: /[<>{}\[\]|\\^~]/, message: "cannot contain special characters" },
                     length: { minimum: 8, maximum: 100 }
   validates :description, presence: true,
-                          format: { with: /[<>{}\[\]|\\^~]/, message: "cannot contain special characters" },
+                          format: { without: /[<>{}\[\]|\\^~]/, message: "cannot contain special characters" },
                           length: { minimum: 40, maximum: 1000 }
   validates :location, presence: true,
-                       format: { with: /[<>{}\[\]|\\^~]/, message: "cannot contain special characters" },
+                       format: { without: /[<>{}\[\]|\\^~]/, message: "cannot contain special characters" },
                        length: { minimum: 4, maximum: 100 }
   validate :start_time_constraint
   validates :max_participants, presence: true, inclusion: { in: 2..8, message: "must be between 2 and 8" }
