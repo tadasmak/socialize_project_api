9d1ba952d330fd3cd4f697b715ca8acd8d2f6fe6
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-26 20:25:39 +0200

    Update test Activity data definition according to Activity model constraints in FactoryBot

--- test/factories/activities.rb
+++ test/factories/activities.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :activity do
-    title { Faker::Lorem.sentence }
-    description { Faker::Lorem.paragraph }
-    location { Faker::Address.city }
-    start_time { Faker::Time.forward(days: 5, period: :evening) }
+    title { Faker::Lorem.paragraph_by_chars(number: rand(8..100)).gsub(/[<>{}\[\]|\\^~]/, "") }
+    description { Faker::Lorem.paragraph_by_chars(number: 256).gsub(/[<>{}\[\]|\\^~]/, "") }
+    location { Faker::Address.city[4..100].gsub(/[<>{}\[\]|\\^~]/, "") }
+    start_time { Faker::Time.between(from: 1.hour.from_now, to: 1.month.from_now) }
     max_participants { rand(2..8) }
 
     association :user
