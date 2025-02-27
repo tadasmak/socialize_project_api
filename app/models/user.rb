2f97a580c0556c9e805fd776d1afc0cea3bc2527
Tadas Maksimavicius <tadasmak@gmail.com>
2025-02-27 21:18:06 +0200

    Generate devise User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\.[^@\s]+\z/
 
   has_many :created_activities, class_name: "Activity", foreign_key: "user_id"
