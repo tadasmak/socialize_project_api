1fda9a629426335b417532054d2c050d9cd72343
Tadas Maksimavicius <tadasmak@gmail.com>
2025-03-03 17:28:23 +0200

    Configure Devise SessionsController for API

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::SessionsController < Devise::SessionsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render status: :ok, json: { message: "Logged in successfully", user: resource }
+  end
+
+  def respond_to_on_destroy
+    if current_user
+      render status: :ok, json: { message: "Logged out successfully" }
+    else
+      render status: :unauthorized, json: { message: "User not found" }
+    end
+  end
+end
