2ec07cd6b9851d105112941a886406353aaf3759
Tadas Maksimavicius <tadasmak@gmail.com>
2025-03-03 17:28:36 +0200

    Set up routes for Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,6 +13,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      devise_for :users, controllers: {
+        sessions: "api/v1/sessions",
+        registrations: "api/v1/registrations"
+      }
+
       resources :activities, only: [ :index, :show, :create, :update, :destroy ]
       resources :participants, only: [ :create ]
       resources :users, only: [ :show, :create, :update, :destroy ]
