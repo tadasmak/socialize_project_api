247aee7c4b1796d4bcf6181b1ca57d0cd72d5095
Tadas Maksimavicius <tadasmak@gmail.com>
2025-03-03 19:14:58 +0200

    Disable session storage in Devise for stateless JWT authentication

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -97,7 +97,7 @@ Devise.setup do |config|
   # Notice that if you are skipping storage for all authentication paths, you
   # may want to disable generating routes to Devise's sessions controller by
   # passing skip: :sessions to `devise_for` in your config/routes.rb
-  config.skip_session_storage = [:http_auth]
+  config.skip_session_storage = [ :http_auth, :params_auth ]
 
   # By default, Devise cleans up the CSRF token on authentication to
   # avoid CSRF token fixation attacks. This means that, when using AJAX
@@ -263,7 +263,7 @@ Devise.setup do |config|
   # should add them to the navigational formats lists.
   #
   # The "*/*" below is required to match Internet Explorer requests.
-  # config.navigational_formats = ['*/*', :html, :turbo_stream]
+  config.navigational_formats = []
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
@@ -313,5 +313,9 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.dig(:jwt, :secret)
+  end
+
+  config.warden do |manager|
+    manager.scope_defaults :user, store: false
   end
 end
