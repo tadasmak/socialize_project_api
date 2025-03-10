d1dfa9873df8a4c0bf7b08599eed0b8932984a4a
Tadas Maksimavicius <tadasmak@gmail.com>
2025-03-10 22:55:16 +0200

    Redefine devise authentication routes to prevent automatic prefixes

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,16 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      devise_for :users, controllers: {
+      devise_for :users, skip: :all, controllers: {
         sessions: "api/v1/sessions",
         registrations: "api/v1/registrations"
       }
+
+      devise_scope :user do
+        post "/users/sign_in", to: "sessions#create"
+        delete "/users/sign_out", to: "sessions#destroy"
+        post "/users", to: "registrations#create"
+      end
 
       resources :activities, only: [ :index, :show, :create, :update, :destroy ]
       resources :participants, only: [ :create ]
