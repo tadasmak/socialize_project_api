e1f5e35880b496c99a9b1260877d6ca413535f26
Tadas Maksimavicius <tadasmak@gmail.com>
2025-03-12 16:39:07 +0200

    Generate authentications_spec file for testing JWT authentication

---
+++ spec/requests/api/v1/authentications_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "User Authentication", type: :request do
+  let(:user) { create(:user) }
+
+  describe "POST /api/v1/users/sign_in" do
+    
+  end
+
+  describe "POST /api/v1/users/sign_out" do
+    
+  end
+end
