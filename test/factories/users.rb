8aaf506a77f8f295711efef3060622df8ea22bd6
Tadas Maksimavicius <tadasmak@gmail.com>
2025-03-12 16:20:53 +0200

    Include password field into FactoryBot's user data

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     username { Faker::Internet.username }
+    password { Faker::Internet.password }
     personality { rand(1..7) }
   end
 end
