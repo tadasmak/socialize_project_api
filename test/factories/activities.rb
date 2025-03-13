d2794fb00414de0c1c54b98e7e07be9ecb96ed3a
Tadas Maksimavicius <tadasmak@gmail.com>
2025-03-13 15:12:21 +0200

    Fix activities FactoryBot location data

--- test/factories/activities.rb
+++ test/factories/activities.rb
@@ -2,7 +2,15 @@ FactoryBot.define do
   factory :activity do
     title { Faker::Lorem.paragraph_by_chars(number: rand(8..100)).gsub(/[<>{}\[\]|\\^~]/, "") }
     description { Faker::Lorem.paragraph_by_chars(number: 256).gsub(/[<>{}\[\]|\\^~]/, "") }
-    location { Faker::Address.city[4..100].gsub(/[<>{}\[\]|\\^~]/, "") }
+
+    location do
+      city = Faker::Address.city
+      while city.length < 4 || city.length > 100
+        city = Faker::Address.city
+      end
+      city.gsub(/[<>{}\[\]|\\^~]/, "")
+    end
+
     start_time { Faker::Time.between(from: 1.hour.from_now, to: 1.month.from_now) }
     max_participants { rand(2..8) }
 
