4a4125bb66c0ef90fbf129517ba8faa68fe3cae4
Tadas Maksimavicius <tadasmak@gmail.com>
2025-03-17 17:34:07 +0200

    Modify users_controller to have set_user method and fix the actions according to it

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,24 +1,29 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :update, :destroy ]
+
   def show
-    user = User.find(params[:id])
-    render status: :ok, json: user
+    render status: :ok, json: @user
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_update_params)
+    @user.update(user_update_params)
 
-    render status: :ok, json: user
+    render status: :ok, json: @user
   end
 
   def destroy
-    user = User.find(params[:id])
-    user.delete
+    @user.destroy
 
     render status: :no_content
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render status: :not_found, json: { error: "User not found" }
+  end
 
   def user_update_params
     params.permit([ :personality, :username ])
