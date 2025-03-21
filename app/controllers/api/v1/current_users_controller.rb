a7129852361100b92f85944c0efcc51ce084f782
Tadas Maksimavicius <tadasmak@gmail.com>
2025-03-21 17:41:15 +0200

    Move user resource's update and destroy actions into CurrentUsersController

--- app/controllers/api/v1/current_users_controller.rb
+++ app/controllers/api/v1/current_users_controller.rb
@@ -1,2 +1,33 @@
 class Api::V1::CurrentUsersController < ApplicationController
+  def show
+    if current_user
+      render status: :ok, json: current_user
+    else
+      render status: :unauthorized, json: { error: "No logged in user" }
+    end
+  end
+
+  def update
+    if current_user.update(user_update_params)
+      render statys: :ok, json: current_user
+    else
+      render status: :unprocessable_entity, json: { errors: current_user.errors }
+    end
+  end
+
+  def destroy
+    sign_out(current_user)
+
+    if current_user.destroy
+      render status: :no_content
+    else
+      render status: :unprocessable_entity, json: { errors: current_user.errors }
+    end
+  end
+
+  private
+
+  def user_update_params
+    params.permit([ :personality, :username ])
+  end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,25 +1,8 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [ :show, :update, :destroy ]
-  before_action -> { authorize_user!(current_user) }, only: [ :update, :destroy ]
+  before_action :set_user, only: [ :show ]
 
   def show
     render status: :ok, json: @user
-  end
-
-  def update
-    if current_user.update(user_update_params)
-      render statys: :ok, json: current_user
-    else
-      render status: :unprocessable_entity, json: { errors: current_user.errors }
-    end
-  end
-
-  def destroy
-    if current_user.destroy
-      render status: :no_content
-    else
-      render status: :unprocessable_entity, json: { errors: current_user.errors }
-    end
   end
 
   private
@@ -28,9 +11,5 @@ class Api::V1::UsersController < ApplicationController
     @user = User.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     render status: :not_found, json: { error: "User not found" }
-  end
-
-  def user_update_params
-    params.permit([ :personality, :username ])
   end
 end
