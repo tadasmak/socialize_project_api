a83fff480302cf04e929e4199b99e52d6d150fbb
Tadas Maksimavicius <tadasmak@gmail.com>
2025-03-24 20:53:28 +0200

    Add a TODO for reworking user_spec.rb tests

--- spec/requests/api/v1/current_users_spec.rb
+++ spec/requests/api/v1/current_users_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+# TODO: Move tests from users_spec.rb to current_user_spec.rb
+
 RSpec.describe "Api::V1::CurrentUsers", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
