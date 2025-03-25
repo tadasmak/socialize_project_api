0898a3dce581a02759101296f414101147760014
Tadas Maksimavicius <tadasmak@gmail.com>
2025-03-25 19:41:25 +0200

    Add test for GET /current_user endpoint

--- spec/requests/api/v1/current_users_spec.rb
+++ spec/requests/api/v1/current_users_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 # TODO: Move tests from users_spec.rb to current_user_spec.rb
 
 RSpec.describe "Api::V1::CurrentUsers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:current_user) { create(:user) }
+  let!(:valid_token) { sign_in(current_user) }
+
+  describe "GET /api/v1/current_user" do
+    it "returns currently logged in user's data" do
+      get api_v1_current_user_path(current_user), headers: { "Authorization" => valid_token }
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)['id']).to eq(current_user.id)
+    end
   end
 end
