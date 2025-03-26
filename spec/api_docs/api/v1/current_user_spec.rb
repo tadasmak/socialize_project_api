82151ad67bfdd9f28b89f0ba946e3b55dca092b1
Tadas Maksimavicius <tadasmak@gmail.com>
2025-03-26 23:47:02 +0200

    Write Swagger spec for PATCH /current_user endpoint

--- spec/api_docs/api/v1/current_user_spec.rb
+++ spec/api_docs/api/v1/current_user_spec.rb
@@ -16,5 +16,22 @@ RSpec.describe 'api/v1/current_user', type: :request do
 
       response(401, 'Unauthorized') { run_test! }
     end
+
+    patch('Update currently logged in user data') do
+      tags 'Session'
+      consumes 'application/json'
+      produces 'application/json'
+
+      security [ BearerAuth: [] ]
+
+      response(200, 'Successful') do
+        schema '$ref' => '#/components/schemas/User'
+        run_test!
+      end
+
+      response(400, 'Bad Request - missing or invalid fields') { run_test! }
+
+      response(401, 'Unauthorized') { run_test! }
+    end
   end
 end
