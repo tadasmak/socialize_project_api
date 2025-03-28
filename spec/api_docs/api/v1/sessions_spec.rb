936120c106dba95140a2f9ee82c14edce3c4a038
Tadas Maksimavicius <tadasmak@gmail.com>
2025-03-28 15:32:47 +0200

    Write Swagger spec for Devise's POST /users/sign_in endpoint

--- spec/api_docs/api/v1/sessions_spec.rb
+++ spec/api_docs/api/v1/sessions_spec.rb
@@ -1,5 +1,36 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/sessions', type: :request do
-  
+  path 'api/v1/users/sign_in' do
+    post('Sign in with user credentials') do
+      tags 'Authentication'
+      consumes 'application/json'
+      produces 'application/json'
+
+      security [ BearerAuth: [] ]
+
+      parameter name: :credentials, in: :body, required: true, schema: {
+        type: :object,
+        properties: {
+          email: { type: :string, example: 'user@example.com' },
+          password: { type: :string, example: 'password123' }
+        },
+        required: %w[email password]
+      }
+
+      response(200, 'Successful') do
+        schema type: :object,
+               properties: {
+                message: { type: :string, example: 'Logged in successfully' },
+                user: { '$ref' => '#/components/schemas/User' }
+               }
+        run_test!
+      end
+
+      response(401, 'Unauthorized') do
+        schema type: :string, example: 'Invalid Email or password'
+        run_test!
+      end
+    end
+  end
 end
