e43ce1e9f647702668dde8596dabc87ba5ffc7df
Tadas Maksimavicius <tadasmak@gmail.com>
2025-04-01 14:01:54 +0300

    Set up ActivitiesConcern methods to process activities filtering/ordering privately

--- app/controllers/concerns/activities_concern.rb
+++ app/controllers/concerns/activities_concern.rb
@@ -1,3 +1,12 @@
 module ActivitiesConcern
-  
+  extend ActiveSupport::Concern
+
+  def gather_activities
+    process_activities(params)
+  end
+
+  private
+
+  def process_activities(params)
+  end
 end
