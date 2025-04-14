ca9a180183045f093ba8568e2ac7adda2a6b40c1
Tadas Maksimavicius <tadasmak@gmail.com>
2025-04-14 18:41:15 +0300

    Fix activity's max participants constraint in participant model into validate instead of before_create for more consistency

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,12 +2,9 @@ class Participant < ApplicationRecord
   belongs_to :activity
   belongs_to :user
 
-  before_create :check_participants_limit
+  validate :activity_participants_limit
 
-  def check_participants_limit
-    if activity.participants.count >= activity.max_participants
-      errors.add(:base, "Maximum participants number reached")
-      throw(:abort)
-    end
+  def activity_participants_limit
+    errors.add(:base, "Maximum participants number reached") if activity.participants.count >= activity.max_participants
   end
 end
