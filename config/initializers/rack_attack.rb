a344886dcba796dba1e685afe2437c31b7c69aa7
Tadas Maksimavicius <tadasmak@gmail.com>
2025-04-14 16:45:59 +0300

    Change POST /activities API to be limitted to 5 requests per 1 minute

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,14 +1,14 @@
 class Rack::Attack
   # Throttle requests to POST /api/v1/activities to 5 requests per minute per user
-  throttle('limit_activity_creation', limit: 1, period: 1.minute) do |req|
-    if req.path == '/api/v1/activities' && req.post?
+  throttle("limit_activity_creation", limit: 5, period: 1.minute) do |req|
+    if req.path == "/api/v1/activities" && req.post?
       # Use the user's ID or IP address as the identifier
-      req.env['warden'].user&.id || req.ip
+      req.env["warden"].user&.id || req.ip
     end
   end
 
   # Custom response for throttled requests
   self.throttled_response = lambda do |env|
-    [429, { 'Content-Type' => 'application/json' }, [{ error: 'Rate limit exceeded. Try again later.' }.to_json]]
+    [ 429, { "Content-Type" => "application/json" }, [ { error: "Rate limit exceeded. Try again later." }.to_json ] ]
   end
 end
