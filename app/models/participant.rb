9e98cd0d5267b1444afdd0aaf366fc3bbe3d5806
Tadas Maksimavicius <tadasmak@gmail.com>
2025-04-14 18:43:12 +0300

    Add maximum activities joined for a user validation into participant model

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -3,8 +3,13 @@ class Participant < ApplicationRecord
   belongs_to :user
 
   validate :activity_participants_limit
+  validate :user_joined_activities_limit
 
   def activity_participants_limit
     errors.add(:base, "Maximum participants number reached") if activity.participants.count >= activity.max_participants
+  end
+
+  def user_joined_activities_limit
+    errors.add(:base, "You can join only up to #{user.max_joined_activities_count} activities") if user.joined_activities.count >= user.max_joined_activities_count
   end
 end
