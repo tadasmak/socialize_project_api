c4b7f276d6a5c51b45b888fd98ae84691ccab04a
Tadas Maksimavicius <tadasmak@gmail.com>
2025-04-15 23:48:24 +0300

    Limit POST /users/sign_in API endpoint to 5 requests per 2 minutes

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -12,6 +12,12 @@ class Rack::Attack
     end
   end
 
+  throttle("limit_sign_in_requests", limit: 5, period: 2.minutes) do |req|
+    if req.path == "/api/v1/users/sign_in" && req.post?
+      req.env["warden"].user&.id || req.ip
+    end
+  end
+
   # Custom response for throttled requests
   self.throttled_response = lambda do |env|
     match_data = env["rack.attack.match_data"]
