91a94e8ae00e6e91b2efe3b5b49d2bf3d9612559
Tadas Maksimavicius <tadasmak@gmail.com>
2025-04-15 23:47:19 +0300

    Add specified rate limit exceeded error messages to different endpoints

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,7 +1,9 @@
 class Rack::Attack
+  # Throttle all requests to 100 requests per minute by IP address
   throttle("limit_all_requests", limit: 100, period: 1.minute) do |req|
     req.env["warden"].user&.id || req.ip
   end
+
   # Throttle requests to POST /api/v1/activities to 5 requests per minute per user
   throttle("limit_activity_creation", limit: 5, period: 1.minute) do |req|
     if req.path == "/api/v1/activities" && req.post?
@@ -12,6 +14,11 @@ class Rack::Attack
 
   # Custom response for throttled requests
   self.throttled_response = lambda do |env|
-    [ 429, { "Content-Type" => "application/json" }, [ { error: "Rate limit exceeded. Try again later." }.to_json ] ]
+    match_data = env["rack.attack.match_data"]
+
+    case match_data[:name]
+    when "limit_all_requests", "limit_activity_creation"
+      [ 429, { "Content-Type" => "application/json" }, [ { error: "Rate limit exceeded. Try again later." }.to_json ] ]
+    end
   end
 end
