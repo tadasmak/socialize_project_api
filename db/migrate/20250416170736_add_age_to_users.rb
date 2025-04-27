8ce3f7afbb2c9f023d04afef678948b55fae8963
Tadas Maksimavicius <tadasmak@gmail.com>
2025-04-27 23:57:57 +0300

    Set User.age to not be nullable

--- db/migrate/20250416170736_add_age_to_users.rb
+++ db/migrate/20250416170736_add_age_to_users.rb
@@ -1,5 +1,13 @@
 class AddAgeToUsers < ActiveRecord::Migration[8.0]
-  def change
+  def up
     add_column :users, :age, :integer
+
+    User.update_all(age: 18)
+
+    change_column_null :users, :age, false
+  end
+
+  def down
+    remove_column :users, :age
   end
 end
