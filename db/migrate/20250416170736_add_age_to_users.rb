c06aa61a9759f643667f2699260dc83b97d13d6d
Tadas Maksimavicius <tadasmak@gmail.com>
2025-04-28 00:28:12 +0300

    Update User.age migration by modern standards

--- db/migrate/20250416170736_add_age_to_users.rb
+++ db/migrate/20250416170736_add_age_to_users.rb
@@ -1,13 +1,12 @@
 class AddAgeToUsers < ActiveRecord::Migration[8.0]
-  def up
-    add_column :users, :age, :integer
+  def change
+    add_column :users, :age, :integer, null: true
 
-    User.update_all(age: 18)
-
-    change_column_null :users, :age, false
-  end
-
-  def down
-    remove_column :users, :age
+    reversible do |dir|
+      dir.up do
+        User.where(age: nil).update_all(age: 18)
+        change_column_null :users, :age, false
+      end
+    end
   end
 end
