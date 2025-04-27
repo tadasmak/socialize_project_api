3556d149fb89f233b3c45a8638a22cdc8c1b8932
Tadas Maksimavicius <tadasmak@gmail.com>
2025-04-28 00:28:09 +0300

    Update Activity.minimum_age and Activity.maximum_age migration by modern standards

--- db/migrate/20250416150909_add_age_range_to_activities.rb
+++ db/migrate/20250416150909_add_age_range_to_activities.rb
@@ -1,16 +1,16 @@
 class AddAgeRangeToActivities < ActiveRecord::Migration[8.0]
   def up
-    add_column :activities, :minimum_age, :integer
-    add_column :activities, :maximum_age, :integer
+    add_column :activities, :minimum_age, :integer, null: true
+    add_column :activities, :maximum_age, :integer, null: true
 
-    Activity.update_all(minimum_age: 18, maximum_age: 100)
+    reversible do |dir|
+      dir.up do
+        Activity.where(minimum_age: nil).update_all(minimum_age: 18)
+        Activity.where(maximum_age: nil).update_all(maximum_age: 100)
 
-    change_column_null :activities, :minimum_age, false
-    change_column_null :activities, :maximum_age, false
-  end
-
-  def down
-    remove_column :activities, :minimum_age
-    remove_column :activities, :maximum_age
+        change_column_null :activities, :minimum_age, false
+        change_column_null :activities, :maximum_age, false
+      end
+    end
   end
 end
