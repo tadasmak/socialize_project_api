8420f3df7487372305c2f4cf38c26e965186c742
Tadas Maksimavicius <tadasmak@gmail.com>
2025-05-19 20:09:25 +0300

    Fix syntax

--- db/migrate/20250210115619_create_participants.rb
+++ db/migrate/20250210115619_create_participants.rb
@@ -7,6 +7,6 @@ class CreateParticipants < ActiveRecord::Migration[8.0]
       t.timestamps
     end
 
-    add_index :participants, [:activity_id, :user_id], unique: true
+    add_index :participants, [ :activity_id, :user_id ], unique: true
   end
 end
