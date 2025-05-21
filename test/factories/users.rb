91de1bcd15a0c84f82d53290d41a91806baa2d1a
Tadas Maksimavicius <tadasmak@gmail.com>
2025-05-22 00:24:28 +0300

    Update FactoryBot users test data with birth_date

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     username { Faker::Internet.username }
     password { Faker::Internet.password }
     personality { rand(1..7) }
+    birth_date { Faker::Date.birthday(min_age: 18, max_age: 100) }
   end
 end
