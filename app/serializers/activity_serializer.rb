0c010aa8f2c36bd9f1557e5b53cec50258298f69
Tadas Maksimavicius <tadasmak@gmail.com>
2025-06-02 15:57:11 +0300

    Remove redundant serializer method for Activity.participants

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -2,8 +2,4 @@ class ActivitySerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :location, :start_time, :max_participants, :minimum_age, :maximum_age, :created_at, :creator, :participants
   belongs_to :creator, serializer: UserSerializer
   has_many :participants, serializer: UserSerializer
-
-  def participants
-    object.users
-  end
 end
