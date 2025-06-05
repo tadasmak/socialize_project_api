1bde9e04389f28f5182de4190f9b4545b3e0a73f
Tadas Maksimavicius <tadasmak@gmail.com>
2025-06-05 14:35:49 +0300

    Separate Activity serializers for list and detailed views

---
+++ app/serializers/activity_detail_serializer.rb
@@ -0,0 +1,5 @@
+class ActivityDetailSerializer < ActiveModel::Serializer
+  attributes :id, :title, :description, :location, :start_time, :max_participants, :minimum_age, :maximum_age, :created_at, :creator, :participants
+  belongs_to :creator, serializer: UserSerializer
+  has_many :participants, serializer: UserSerializer
+end
--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,5 +1,9 @@
 class ActivitySerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :location, :start_time, :max_participants, :minimum_age, :maximum_age, :created_at, :creator, :participants
+  attributes :id, :title, :description, :location, :start_time, :max_participants, :minimum_age, :maximum_age, :created_at, :creator, :participants_count
   belongs_to :creator, serializer: UserSerializer
   has_many :participants, serializer: UserSerializer
+
+  def participants_count
+    participants.count
+  end
 end
