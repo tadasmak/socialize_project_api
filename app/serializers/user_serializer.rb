76a417260dba1f85f3e6bdc4ab3409e5a0629e37
Tadas Maksimavicius <tadasmak@gmail.com>
2025-06-06 19:38:38 +0300

    Return text data for personality in UserSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :username, :personality, :age
+
+  def personality
+    object.personality&.tr("_", " ").capitalize
+  end
 end
