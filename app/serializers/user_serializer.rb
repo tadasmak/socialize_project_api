48bca4485b1141aa4a7c63dfbef3ce299cf4f14d
Tadas Maksimavicius <tadasmak@gmail.com>
2025-06-10 16:37:01 +0300

    Fix a bug for personality serialization method in UserSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :username, :personality, :age
 
   def personality
-    object.personality&.tr("_", " ").capitalize
+    object.personality&.tr("_", " ")&.capitalize
   end
 end
