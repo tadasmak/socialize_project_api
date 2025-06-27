b27dd0ba7c2e828c0242da965b7de885d648b029
Tadas Maksimavicius <tadasmak@gmail.com>
2025-06-27 14:06:57 +0300

    Disable authentication and incoming HTTP requests rate limiter temporarily for frontend development

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,32 +1,32 @@
 class Rack::Attack
-  # Throttle all requests to 100 requests per minute by IP address
-  throttle("limit_all_requests", limit: 100, period: 1.minute) do |req|
-    req.env["warden"].user&.id || req.ip
-  end
+  # # Throttle all requests to 100 requests per minute by IP address
+  # throttle("limit_all_requests", limit: 100, period: 1.minute) do |req|
+  #   req.env["warden"].user&.id || req.ip
+  # end
 
-  # Throttle requests to POST /api/v1/activities to 5 requests per minute per user
-  throttle("limit_activity_creation", limit: 5, period: 1.minute) do |req|
-    if req.path == "/api/v1/activities" && req.post?
-      # Use the user's ID or IP address as the identifier
-      req.env["warden"].user&.id || req.ip
-    end
-  end
+  # # Throttle requests to POST /api/v1/activities to 5 requests per minute per user
+  # throttle("limit_activity_creation", limit: 5, period: 1.minute) do |req|
+  #   if req.path == "/api/v1/activities" && req.post?
+  #     # Use the user's ID or IP address as the identifier
+  #     req.env["warden"].user&.id || req.ip
+  #   end
+  # end
 
-  throttle("limit_sign_in_requests", limit: 5, period: 5.minutes) do |req|
-    if req.path == "/api/v1/users/sign_in" && req.post?
-      req.env["warden"].user&.id || req.ip
-    end
-  end
+  # throttle("limit_sign_in_requests", limit: 5, period: 5.minutes) do |req|
+  #   if req.path == "/api/v1/users/sign_in" && req.post?
+  #     req.env["warden"].user&.id || req.ip
+  #   end
+  # end
 
-  # Custom response for throttled requests
-  self.throttled_response = lambda do |env|
-    throttle_name = env["rack.attack.matched"]
+  # # Custom response for throttled requests
+  # self.throttled_response = lambda do |env|
+  #   throttle_name = env["rack.attack.matched"]
 
-    case throttle_name
-    when "limit_sign_in_requests"
-      [ 429, { "Content-Type" => "application/json" }, [ { error: "Too many sign-in attempts. Please try again later." }.to_json ] ]
-    else
-      [ 429, { "Content-Type" => "application/json" }, [ { error: "Rate limit exceeded. Try again later." }.to_json ] ]
-    end
-  end
+  #   case throttle_name
+  #   when "limit_sign_in_requests"
+  #     [ 429, { "Content-Type" => "application/json" }, [ { error: "Too many sign-in attempts. Please try again later." }.to_json ] ]
+  #   else
+  #     [ 429, { "Content-Type" => "application/json" }, [ { error: "Rate limit exceeded. Try again later." }.to_json ] ]
+  #   end
+  # end
 end
