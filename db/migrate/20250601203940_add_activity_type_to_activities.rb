30ca513ebffa43e840e0f6915ad1a23154bf373e
Tadas Maksimavicius <tadasmak@gmail.com>
2025-07-12 18:01:23 +0300

    Rename migration of adding activity_type to Activity correctly

--- db/migrate/20250601203940_add_activity_type_to_activities.rb
+++ db/migrate/20250601203940_add_activity_type_to_activities.rb
@@ -1,4 +1,4 @@
-class AddTypeToActivities < ActiveRecord::Migration[8.0]
+class AddActivityTypeToActivities < ActiveRecord::Migration[8.0]
   def change
     add_column :activities, :activity_type, :integer, null: false, default: 0
   end
