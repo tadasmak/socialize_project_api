4c6a47075c28160078e59f0959c3919b2974c7b0
Tadas Maksimavicius <tadasmak@gmail.com>
2025-07-12 19:22:33 +0300

    Set full activities status as "full" on Activity.status migration

--- db/migrate/20250712150611_add_status_to_activities.rb
+++ db/migrate/20250712150611_add_status_to_activities.rb
@@ -1,5 +1,17 @@
 class AddStatusToActivities < ActiveRecord::Migration[8.0]
   def change
     add_column :activities, :status, :string, default: "open", null: false
+
+    reversible do |dir|
+      dir.up do
+        Activity.reset_column_information
+
+        Activity.find_each do |activity|
+          if activity.max_participants.present? && activity.participant_records.count >= activity.max_participants
+            activity.update_column(:status, "full")
+          end
+        end
+      end
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_01_203940) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_12_150611) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_01_203940) do
     t.integer "minimum_age", null: false
     t.integer "maximum_age", null: false
     t.integer "activity_type", default: 0, null: false
+    t.string "status", default: "open", null: false
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
