d6a4c4cfe18359f4df692312d7c1755304cf1af7
Tadas Maksimavicius <tadasmak@gmail.com>
2025-07-15 23:35:44 +0300

    Add status handling to ActivityStatusManager service object

--- app/services/activity_status_manager.rb
+++ app/services/activity_status_manager.rb
@@ -4,19 +4,31 @@ class ActivityStatusManager
   end
 
   def mark_as_full
-    @activity.update(status: "full") if can_be_marked_full?
+    return :ineligible unless can_be_marked_full?
+    return :invalid unless @activity.update(status: :full)
+
+    :success
   end
 
   def mark_as_open
-    @activity.update(status: "open") if can_be_marked_open?
+    return :ineligible unless can_be_marked_open?
+    return :invalid unless @activity.update(status: :open)
+
+    :success
   end
 
   def mark_as_confirmed
-    @activity.update(status: "confirmed") if can_be_marked_confirmed?
+    return :ineligible unless can_be_marked_confirmed?
+    return :invalid unless @activity.update(status: :confirmed)
+
+    :success
   end
 
   def mark_as_cancelled
-    @activity.update(status: "cancelled") if can_be_marked_cancelled?
+    return :ineligible unless can_be_marked_cancelled?
+    return :invalid unless @activity.update(status: :cancelled)
+
+    :success
   end
 
   private
