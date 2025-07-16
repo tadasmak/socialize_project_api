ca71b3da430269ab96ece8f4cbf9e69e8a10fd87
Tadas Maksimavicius <tadasmak@gmail.com>
2025-07-16 20:10:22 +0300

    Add POST /activities/:id/cancel endpoint to cancel activity

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
         post "join", on: :member, to: "activities#join"
         delete "leave", on: :member, to: "activities#leave"
         post "confirm", on: :member, to: "activities#set_confirmed_status"
+        post "cancel", on: :member, to: "activities#set_cancelled_status"
 
         post "generate_description", on: :collection, to: "activities#generate_description"
         get "description_status", on: :collection, to: "activities#description_status"
