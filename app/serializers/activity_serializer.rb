04617f55e724b2dd08c87a2942b64765596d2771
Tadas Maksimavicius <tadasmak@gmail.com>
2025-07-19 23:55:23 +0300

    Add minimum_age, maximum_age instead of age_range in ActivitySerializer

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,11 +1,5 @@
 class ActivitySerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :location, :start_time, :max_participants, :age_range, :created_at, :creator, :participants_count
+  attributes :id, :title, :description, :location, :start_time, :max_participants, :minimum_age, :maximum_age, :created_at, :creator, :participants_count
   belongs_to :creator, serializer: UserSerializer
   has_many :participants, serializer: UserSerializer
-
-  def age_range
-    return nil unless object.minimum_age.present? && object.maximum_age.present?
-
-    "#{object.minimum_age}-#{object.maximum_age}"
-  end
 end
