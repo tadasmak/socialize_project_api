08d507abf0c803be4b3f3cac4cb76f12cbc236dc
Tadas Maksimavicius <tadasmak@gmail.com>
2025-07-19 23:17:19 +0300

    Add created_activities and joined_activities objects to CurrentUserSerializer

--- app/serializers/current_user_serializer.rb
+++ app/serializers/current_user_serializer.rb
@@ -1,3 +1,9 @@
 class CurrentUserSerializer < ActiveModel::Serializer
   attributes :id, :email, :username, :personality, :age, :birth_date
+  has_many :created_activities, serializer: ActivitySerializer
+  has_many :joined_activities, serializer: ActivitySerializer
+
+  def joined_activities
+    object.joined_activities.where.not(user_id: object.id).order(created_at: :desc)
+  end
 end
