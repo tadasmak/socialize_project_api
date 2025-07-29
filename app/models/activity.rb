08234f6f13ee2a48eb5937d375cfa5932fd87d63
Tadas Maksimavicius <tadasmak@gmail.com>
2025-07-30 02:15:41 +0300

    Decompose validation methods with multiple responsibilities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -21,6 +21,7 @@ class Activity < ApplicationRecord
   validates :location, presence: true,
                        format: { without: /[<>{}\[\]|\\^~]/, message: "cannot contain special characters" },
                        length: { minimum: 4, maximum: 100 }
+  validates :start_time, presence: true
   validates :max_participants, presence: true,
                                inclusion: { in: 2..8, message: "must be between 2 and 8" }
   validates :minimum_age, presence: true, numericality: { greater_than_or_equal_to: 18,
@@ -29,10 +30,12 @@ class Activity < ApplicationRecord
   validates :maximum_age, presence: true, numericality: { greater_than_or_equal_to: 18,
                                                           less_than_or_equal_to: 100,
                                                           message: "must be between 18 and 100" }
-
-  validate :age_range_logic
+  validate :start_time_cannot_be_in_past
+  validate :start_time_cannot_be_too_far_in_future
+  validate :age_range_order
+  validate :age_range_span
+  validate :creator_within_age_range
   validate :created_activities_per_user_limit, if: :new_record?
-  validate :start_time_constraint
 
   after_update :handle_activity_status
 
@@ -46,10 +49,24 @@ class Activity < ApplicationRecord
 
   private
 
-  def start_time_constraint
-    errors.add(:start_time, "start_time does not exist") if start_time.blank?
-    errors.add(:start_time, "start time should be in the future") if start_time < Time.now
+  def start_time_cannot_be_in_past
+    errors.add(:start_time, "should be in the future") if start_time < Time.current
+  end
+
+  def start_time_cannot_be_too_far_in_future
     errors.add(:start_time, "must be no further that 1 month in the future") if start_time > 1.month.from_now
+  end
+
+  def age_range_inequality
+    errors.add(:minimum_age, "cannot be greater than maximum age") if minimum_age > maximum_age
+  end
+
+  def creator_within_age_range
+    errors.add(:creator, "must be inside the age range") unless age_range.include?(creator.age)
+  end
+
+  def age_range_span
+    errors.add(:age_range, "must be between 4 and 8 years") unless age_range.count >= 5 && age_range.count <= 9
   end
 
   def created_activities_per_user_limit
@@ -57,12 +74,6 @@ class Activity < ApplicationRecord
     max_activities_count = creator.max_upcoming_created_activities_count
 
     errors.add(:base, "You can only create #{max_activities_count} activities that are yet to take place at a time") if activities_count >= max_activities_count
-  end
-
-  def age_range_logic
-    errors.add(:base, "Minimum age cannot be greater than maximum age") if minimum_age > maximum_age
-    errors.add(:base, "Creator must be inside the age range") unless age_range.include?(creator.age)
-    errors.add(:base, "Age range must be between 4 and 8 years") unless age_range.count >= 5 && age_range.count <= 9
   end
 
   def handle_activity_status
