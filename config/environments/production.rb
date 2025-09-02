c1693b4e87161fc09a9d4d6a2ebf839e16b85ac6
Tadas Maksimavicius <tadasmak@gmail.com>
2025-09-02 23:55:58 +0300

    Switch from solid_cache to redis cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,10 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :solid_cache_store
+  config.cache_store = :redis_cache_store, {
+    url: "redis://socialize_project-redis:6379/0",
+    expires_in: 1.hour
+  }
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
 
