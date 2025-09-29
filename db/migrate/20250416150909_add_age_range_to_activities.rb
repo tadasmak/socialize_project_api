cfd40dee0569a6d23cebf6ae39a9a574b7ff6323
Tadas Maksimavicius <tadasmak@gmail.com>
2025-09-29 14:36:46 +0300

    Fix Activity minimum_age and maximum_age faulty migration

--- db/migrate/20250416150909_add_age_range_to_activities.rb
+++ db/migrate/20250416150909_add_age_range_to_activities.rb
@@ -3,14 +3,15 @@ class AddAgeRangeToActivities < ActiveRecord::Migration[8.0]
     add_column :activities, :minimum_age, :integer, null: true
     add_column :activities, :maximum_age, :integer, null: true
 
-    reversible do |dir|
-      dir.up do
-        Activity.where(minimum_age: nil).update_all(minimum_age: 18)
-        Activity.where(maximum_age: nil).update_all(maximum_age: 100)
+    execute "UPDATE activities SET minimum_age = 18 WHERE minimum_age IS NULL"
+    execute "UPDATE activities SET maximum_age = 100 WHERE maximum_age IS NULL"
 
-        change_column_null :activities, :minimum_age, false
-        change_column_null :activities, :maximum_age, false
-      end
-    end
+    change_column_null :activities, :minimum_age, false
+    change_column_null :activities, :maximum_age, false
+  end
+
+  def down
+    remove_column :activities, :minimum_age
+    remove_column :activities, :maximum_age
   end
 end
