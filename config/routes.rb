2e9f4aac5c2165a3e5cfb79068da799d6e60ebdb
Tadas Maksimavicius <tadasmak@gmail.com>
2025-10-15 23:46:16 +0300

    Create /api/v1/cable endpoint for ActionCable connections

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   mount Rswag::Ui::Engine => "/api/v1/docs"
   mount Rswag::Api::Engine => "/api/v1/docs"
+
+  mount ActionCable.server => "/api/v1/cable"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
