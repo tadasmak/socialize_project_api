ab1cae59333bb1704a4c0454df7f0aafe5e1de12
Tadas Maksimavicius <tadasmak@gmail.com>
2025-10-29 15:52:57 +0200

    Add allowed_request_origins for ActionCable in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,11 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  config.action_cable.allowed_request_origins = [
+    "https://socializeproject.com",
+    "https://www.socializeproject.com"
+  ]
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
